bcbe1f5dfb45164a9b0258fbb0270e529002cf80
anuragkaushik10 <ak10anurag@gmail.com>
2022-06-10 15:03:16 +0530

    day2

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -187,7 +187,7 @@ const Sidebar = (props) => {
             </Row>
           </div>
           {/* Navigation */}
-          {/* <Nav navbar>{createLinks(routes)}</Nav> */}
+          <Nav navbar>{createLinks(routes)}</Nav>
         </Collapse>
       </Container>
     </Navbar>
--- src/routes.js
+++ src/routes.js
@@ -22,7 +22,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
-
+import PatientForm from "views/examples/PatientForm.js";
 var routes = [
   {
     path: "/index",
@@ -72,6 +72,13 @@ var routes = [
     icon: "ni ni-circle-08 text-pink",
     component: Register,
     layout: "/auth",
+  },
+  {
+    path: "/patient-form",
+    name: "Patient Form",
+    icon: "ni ni-circle-08 text-pink",
+    component: PatientForm,
+    layout: "/admin",
   },
 ];
 export default routes;
